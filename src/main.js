27d3a73aa699342adfe68ee535b82a6b752514b6
McCullough <zs.flamingos@gmail.com>
2026-03-01 22:06:20 +0800

    fix

--- src/main.js
+++ src/main.js
@@ -3,7 +3,9 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import ElementUI from "element-ui";
-import "./assets/css/index.scss";
+/* 拆分为两处 import，避免 index.scss 单次编译 Element 全量主题导致 heap OOM */
+import "./assets/css/element-theme.scss";
+import "./assets/css/app.scss";
 import enLang from "element-ui/lib/locale/lang/en"; // 如果使用中文语言包请默认支持，无需额外引入，请删除该依赖
 
 Vue.use(ElementUI, {
